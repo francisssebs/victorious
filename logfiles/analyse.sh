4daebefabf7f511ffd970b0f19c5cd8d74a45f72
wallace <francisssebs0@gmail.com>
2025-12-15 13:25:22 -0500

    updated pointchk

--- logfiles/analyse.sh
+++ logfiles/analyse.sh
@@ -5,18 +5,18 @@
 echo "Analysing log files."
 echo " ========================================================= "
 
-grep "ERROR" application.log 
-grep "FATAL" application.log 
-grep "CRITICAL" application.log
+grep "ERROR" /c/Users/user/desktop/wale-dir/nana/logfiles/application.log 
+grep "FATAL" /c/Users/user/desktop/wale-dir/nana/logfiles/application.log 
+grep "CRITICAL" /c/Users/user/desktop/wale-dir/nana/logfiles/application.log
 echo -e "\n number of discovey "
-grep -c "ERROR" application.log
-grep -c "FATAL" application.log
-grep -c "CRITICAL" application.log
+grep -c "ERROR" /c/Users/user/desktop/wale-dir/nana/logfiles/application.log
+grep -c "FATAL" /c/Users/user/desktop/wale-dir/nana/logfiles/application.log
+grep -c "CRITICAL" /c/Users/user/desktop/wale-dir/nana/logfiles/application.log
 echo -e "\n Anlysing system logs "
-grep "ERROR" system.log
-grep "FATAL" system.log
-grep "CRITICAL" system.log
+grep "ERROR" /c/Users/user/desktop/wale-dir/nana/logfiles/system.log
+grep "FATAL" /c/Users/user/desktop/wale-dir/nana/logfiles/system.log
+grep "CRITICAL" /c/Users/user/desktop/wale-dir/nana/logfiles/system.log
 
-grep -c "ERROR" system.log
-grep -c "FATAL" system.log
-grep -c "CRITICAL" system.log
+grep -c "ERROR" /c/Users/user/desktop/wale-dir/nana/logfiles/system.log
+grep -c "FATAL" /c/Users/user/desktop/wale-dir/nana/logfiles/system.log
+grep -c "CRITICAL" /c/Users/user/desktop/wale-dir/nana/logfiles/system.log
